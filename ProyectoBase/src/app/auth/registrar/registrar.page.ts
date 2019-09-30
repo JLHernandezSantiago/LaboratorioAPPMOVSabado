8b216a1fc16b32cdd40459275bf9d101a9e92873
JLHernandezSantiago <jl.hdz.stgo@gmail.com>
2019-09-30 14:05:18 -0500

    Practica login 2.0

--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group(
+      {
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarContrasena: ['', Validators.required]
+    });
+   }
 
   ngOnInit() {
+   this.registerForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarPassword: ['', Validators.required]
+    },
+    {
+      Validator: this.MustMatch('password', 'confirmarPassword')
+    });
   }
 
+
+get f() { return this.registerForm.controls; }
+
+  registrar() {
+      console.log(this.registerForm.valid);
+  }
+
+  MustMatch(controlName: string, matchControlName: string) {
+    return true;
+  }
 }
